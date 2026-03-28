40cca24781c244feba72f958261ada21ee965f7e
RyukGore <juansebastian.wilchesrodriguez@gmail.com>
2026-03-28 16:46:09 -0500

    Cambio base url comunicacion API

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -4,6 +4,8 @@ import { provideHttpClient } from '@angular/common/http';
 
 import { routes } from './app.routes';
 import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
+import { API_BASE_URL } from './tokens/api-base-url.token';
+import { environment } from '../environments/environment';
 
 export const appConfig: ApplicationConfig = {
   providers: [
@@ -11,5 +13,6 @@ export const appConfig: ApplicationConfig = {
     provideRouter(routes),
     provideClientHydration(withEventReplay()),
     provideHttpClient(),
+    { provide: API_BASE_URL, useValue: environment.apiBaseUrl },
   ],
 };
